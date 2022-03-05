9242b5a83d1ce57a703f71181cb53db957d35266
Divaltor <kuklavudu78@gmail.com>
2022-03-06 00:10:40 +0200

    Added missed routes

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 
 from base.api.user import UserCRUDView
+from base.api.user_car import UserCarCRUDView
 
 router = routers.SimpleRouter()
 router.register(r'users', UserCRUDView)
+router.register(r'users-car', UserCarCRUDView)
 
 urlpatterns = [] + router.urls
--- car_management/urls.py
+++ car_management/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 
+from car_management.api.car_brand import CarBrandCRUDView
 from car_management.api.car_model import CarModelCRUDView
 
 router = routers.SimpleRouter()
-router.register(r'car-brands', CarModelCRUDView)
+router.register(r'car-brands', CarBrandCRUDView)
+router.register(r'cars', CarModelCRUDView)
 
 
 urlpatterns = [] + router.urls
